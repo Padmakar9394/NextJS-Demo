b36ba2df8913fd7181878f2d82ea91b5d32ff78d
Padmakar9394 <prg2024btech@gmail.com>
2024-05-03 17:14:57 +0530

    Added responsiveness to the pages

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+import styles from "./singleBlog.module.css"
+
 const SinglePostPage = () => {
     return (
-        <div>
-            Single Post Page
+        <div className={styles.container}>
+            <div className={styles.imageContainer}>
+                <Image src="/contact.png" alt="img" fill className={styles.img} />
+            </div>
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>Title</h1>
+                <div className={styles.details}>
+                    <Image src="/contact.png" alt="user-img" width={50} height={50} className={styles.avatar} />
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Author</span>
+                        <span className={styles.detailValue}>John Snow</span>
+                    </div>
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Published</span>
+                        <span className={styles.detailValue}>01.01.2024</span>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora tenetur maxime odio voluptatibus vel dolorem quam minus, sint earum similique quos non assumenda. Deleniti, maiores ipsa distinctio aspernatur placeat adipisci.
+            </div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/links/Links.js
+++ src/components/Navbar/links/Links.js
@@ -1,64 +1,73 @@
 "use client";
 
-import Link from "next/link"
+import Link from "next/link";
 import styles from "./links.module.css";
 import NavLink from "./navLinks/navLink";
 import { useState } from "react";
+import Image from "next/image";
 
 const links = [
-    {
-        title: "Home",
-        path: "/"
-    },
-    {
-        title: "About",
-        path: "/about"
-    },
-    {
-        title: "Contact",
-        path: "/contact"
-    },
-    {
-        title: "Blog",
-        path: "/blog"
-    },
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
 ];
 
 const Links = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    //TEMPERORY
-    const session = true;
-    const isAdmin = true;
+  //TEMPERORY
+  const session = true;
+  const isAdmin = true;
 
   return (
     <div className={styles.container}>
-        <div className={styles.links}>
-            {links.map((link) => 
-                <NavLink item={link} key={link.title} />
-            )}
-            {
-                session ? (
-                    <>
-                        {isAdmin && <NavLink item={{title: "Admin", path: "/admin"}} />}
-                        <button className={styles.logout}>Logout</button> 
-                    </>
-                ) : (
-                <NavLink item={{title: "Login", path: "/login"}} />
-            )}
-        </div>
-
-        <button className={styles.menuBtn} onClick={() => setOpen((prev) => !prev)}>Menu</button>
-        {
-            open && (
-            <div className={styles.mobileLinks}>
-                { links.map((link) => 
-                    <NavLink item={link} key={link.title} />
-                )}
-            </div>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
         )}
+      </div>
+
+      <Image
+        className={styles.menuBtn}
+        src="/menu.png"
+        alt="menuImg"
+        width={30}
+        height={30}
+        onClick={() => setOpen((prev) => !prev)}
+      />
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink
+              item={link}
+              key={link.title}
+            />
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Links;
